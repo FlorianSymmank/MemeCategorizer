d567c252a556cd3047b4db3bb258335c624deb13
FlorianSymmank <floriansymmank@gmx.de>
2023-05-13 18:34:59 +0200

    template

--- config.py
+++
@@ -1,4 +0,0 @@
-processed_meme_dir = r"F:\VisualStudioProjekte\MemeCategorizer\preprocessed"
-data_dir = f"F:\VisualStudioProjekte\MemeCategorizer\data"
-meme_dir = r"F:\Wichtige Dokumente\Box Sync\Memes"
-tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
---
+++ config_template.py
@@ -0,0 +1,4 @@
+processed_meme_dir = r"\preprocessed"
+data_dir = f"\data"
+meme_dir = r"\Memes" # your meme dir
+tesseract_cmd = "tesseract.exe" # tesseract executable
