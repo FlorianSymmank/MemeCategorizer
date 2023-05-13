dcf4c48113ba402bd0f4fc78de83766fea5cc7d9
FlorianSymmank <floriansymmank@gmx.de>
2023-05-13 18:26:07 +0200

    added config file

---
+++ config.py
@@ -0,0 +1,4 @@
+processed_meme_dir = r"F:\VisualStudioProjekte\MemeCategorizer\preprocessed"
+data_dir = f"F:\VisualStudioProjekte\MemeCategorizer\data"
+meme_dir = r"F:\Wichtige Dokumente\Box Sync\Memes"
+tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
