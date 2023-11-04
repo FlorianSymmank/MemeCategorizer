b79f587ae1841d6bb1a1341a37a655086094b8c6
FlorianSymmank <floriansymmank@gmx.de>
2023-11-04 14:41:48 +0100

    restructured

--- composite_meme.py
+++ composite_meme.py
@@ -7,11 +7,13 @@ import random
 from PIL import Image
 Image.MAX_IMAGE_PIXELS = None # enable operations on large images
 
+fill_color = "mean_color" # "mean_color", "median_color"
+
 def main():
     data_files = os.listdir(config.processed_meme_dir)
     
-    median_color_files = [load_data(os.path.splitext(file)[
-        0]) for file in data_files if "median_color" in load_data(os.path.splitext(file)[0])]
+    data_files = [load_data(os.path.splitext(file)[
+        0]) for file in data_files if fill_color in load_data(os.path.splitext(file)[0])]
     
     # create pointcloud
     path = "colors.xyz"
@@ -28,28 +30,33 @@ def main():
         # f.write("255 255 0 \n")
         # f.write("255 255 255 \n")
         #endregion
-        for data in median_color_files:
-            f.write(" ".join(str(x) for x in data["median_color"]) +"\n")
+        for data in data_files:
+            f.write(" ".join(str(x) for x in data[fill_color]) +"\n")
 
     pcd = o3d.io.read_point_cloud(path)
     # visualize(pcd)
     pcd_tree = o3d.geometry.KDTreeFlann(pcd)
 
+    create_image("snake.jpg", "snake_300_300_n10.png", (300,300), pcd_tree, pcd, data_files)
 
+
+def create_image(src, dest, sub_img_size, pcd_tree, pcd, data_files):
     # define new image dimensions
-    sub_image_width = sub_image_height = 400
-    img = Image.open('snake.jpg')
+    (sub_image_width, sub_image_height) = sub_img_size
+    img = Image.open(src)
     img = img.convert("RGB")
     pixels = img.load()
     width, height = img.size
+
+    print(f"New image size: W={width*sub_image_width} H={height*sub_image_height}")
 
     big_img = Image.new('RGB', (width*sub_image_width, height*sub_image_height), (255, 255, 255))
 
     # for each pixel, find matching meme
     for y in range(height):
-        print(y) # some progress bar ig
+        print(f"Row: {y + 1}/{height}") # some progress bar ig
         for x in range(width):
-            meme = find_matching_meme(pixels[x, y], pcd_tree, pcd, median_color_files)
+            meme = find_matching_meme(pixels[x, y], pcd_tree, pcd, data_files)
 
             file = "median_" + meme["id"] + ".tiff"
             path = os.path.join(config.padded_meme_dir, file)
@@ -57,16 +64,18 @@ def main():
             sub_img = Image.open(path)
             big_img.paste(sub_img, (x * sub_image_width, y * sub_image_height))
 
-    big_img.save('result10.png')
+    print("Save Image (could take a while ...)")
+    big_img.save(dest)
 
-def find_matching_meme(coord, pcd_tree, pcd, median_color_files):
+
+def find_matching_meme(coord, pcd_tree, pcd, data_files):
     n = 10
     k, idx, _ = pcd_tree.search_knn_vector_3d(coord, n)
     points = np.asarray(pcd.points)
 
     r = random.randint(0, n-1)
     color = points[idx[r]]
-    roughly_equal_meme = [x for x in median_color_files if (np.array(x["median_color"]) == np.array(color)).all()]
+    roughly_equal_meme = [x for x in data_files if (np.array(x[fill_color]) == np.array(color)).all()]
     return roughly_equal_meme[0]
 
 
